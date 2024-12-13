a6adb4c14348789bc8f95fb513b665797f619f33
Daya Nand <nandd1224@gmail.com>
2024-12-13 11:22:34 +0500

    Add files via upload

---
+++ task.py
@@ -0,0 +1,11 @@
+
+a=input("enter value of a: ")
+b=input("enter value of b: ")
+a=int(a)
+b=int(b)
+print(f"value of a is {a}")
+print(f"value of b is {b}")
+print("sum is " +str(a+b))
+print("Difference is " +str(a-b))
+print("product is " +str(a*b))
+print("division is " +str(a/b))
